ce905190e2ecaf3d85deeae57008a123bb224617
Hirak <153591644+HiraK7dev@users.noreply.github.com>
2024-10-15 11:51:54 +0530

    fixed some bugs

--- controllers/deleteTodo.js
+++ controllers/deleteTodo.js
@@ -2,11 +2,15 @@ import { Todo } from "../models/todos.models.js";
 
 async function deleteTodo(req, res) {
     const { id } = req.params;
-    try {
-        const result = await Todo.deleteOne({ "_id": id });
-        res.status(200).json({ message: "success" });
-    } catch (error) {
-        res.status(500).json({ message: "error" });
+    if((await Todo.find({ "_id": id })).length) {
+        try {
+            const result = await Todo.deleteOne({ "_id": id });
+            res.status(200).json({ message: "success" });
+        } catch (error) {
+            res.status(500).json({ message: "error" });
+        }
+    } else {
+        res.status(404).json({ message: "not found" });
     }
 }
 
--- controllers/editTodo.js
+++ controllers/editTodo.js
@@ -3,14 +3,18 @@ import { Todo } from "../models/todos.models.js";
 async function editTodo(req, res) {
     const { id } = req.params;
     const body = req.body;
-    try {
-        const result = await Todo.updateOne(
-            { "_id": id },
-            { $set: { "task": body.task, "isCompleted": body.isCompleted }}
-        );
-        res.status(200).json({ message: "success" });
-    } catch (error) {
-        res.status(500).json({ message: "error" });
+    if ((await Todo.find({ "_id": id })).length) {
+        try {
+            const result = await Todo.updateOne(
+                { "_id": id },
+                { $set: { "task": body.task, "isCompleted": body.isCompleted } }
+            );
+            res.status(200).json({ message: "success" });
+        } catch (error) {
+            res.status(500).json({ message: "error" });
+        }
+    } else {
+        res.status(404).json({ message: "not found" });
     }
 }
 
